659cff744783b4ea5dce9830e0f9feb569c89cd7
Sai Sasank Vindamuri <imsasankvindamuri@gmail.com>
2024-08-09 15:21:04 +0530

    Created a basic timer.

--- Pomodoro_Website/pomodorofunctions.js
+++ Pomodoro_Website/pomodorofunctions.js
@@ -1,42 +1,62 @@
 (function () {
     var secondsElapsed = 0;
-    var _timer = null;
+    var timeMinsFocus = 25, timeMinsBreak = 5, timeMinsLongBreak = 15;
+
+
+
+    var _timer = function (timeMins ,timeSecs = 0) {
+        setInterval(function () {
+            if (timeSecs < 0){  
+                if (timeMins == 0){
+                    clearInterval(_timer);
+                    return;
+                }
+                timeMins--;
+                timeSecs = 59;
+            }
+            const timeStamp = `${timeMins.toString().padStart(2,"0")} : ${timeSecs.toString().padStart(2,"0")}`;
+            document.getElementById("time").innerHTML = timeStamp;
+            timeSecs--;
+        }, 1000);
+    }
+
+
     const secondFunc = function () {
         console.log(secondsElapsed);
     };
     const focustime = function () {
         
-        if (_timer) {
-            return;
-        }
-        console.log("focus");
-        _timer = setInterval(function () {
-            secondsElapsed++;
-            secondFunc();
-        },1000);
+        // if (_timer) {
+        //     return;
+        // }
+        // console.log("focus");
+        // _timer = setInterval(function () {
+        //     secondsElapsed++;
+        //     secondFunc();
+        // },1000);
 
-        
-        document.getElementById("time-indicator").innerHTML = 'focus time.'
+        _timer(timeMinsFocus); 
+        document.getElementById("section-indicator").innerHTML = 'focus time.'
     };
     const breaktime = function () {
         
-        clearInterval(_timer);
-        _timer = null;
-        secondsElapsed = 0;
+        // clearInterval(_timer);
+        // _timer = null;
+        // secondsElapsed = 0;
 
 
-        console.log("break");
-        document.getElementById("time-indicator").innerHTML = 'break time.'
+        // console.log("break");
+        document.getElementById("section-indicator").innerHTML = 'break time.'
     };
     const longbreaktime = function () {
         
-        clearInterval(_timer);
-        _timer = null;
-        secondsElapsed = 0;
+        // clearInterval(_timer);
+        // _timer = null;
+        // secondsElapsed = 0;
 
 
-        console.log("long break");
-        document.getElementById("time-indicator").innerHTML = 'long break time.'
+        // console.log("long break");
+        document.getElementById("section-indicator").innerHTML = 'long break time.'
     };
 
     const _pomo = {
@@ -44,8 +64,6 @@
         breaktime,
         longbreaktime,
     }
-
-    console.log(_pomo);
 
 
     document.getElementById("focustime").addEventListener("click",_pomo.focustime);
